35c7697c5dfe8547decc6d53b49968842778b0e5
chominseo0723 <singer100@yu.ac.kr>
2025-07-03 17:38:56 +0900

    1번째 js 프로그래머스 문제풀기

---
+++ JS/index.js
@@ -0,0 +1,7 @@
+// 서울에서 김서방 찾기 프로그래머스 Lv1 js 연습
+
+// function solution(seoul) {
+//     let index = seoul.indexOf("Kim")
+//     const message = "김서방은 " + index + "에 있다"
+//     return message
+// }
