2a6cf717c29f2eecea1d2a2d830e604090f487a2
chominseo0723 <singer100@yu.ac.kr>
2025-07-04 15:41:11 +0900

    Tailwind 적용

---
+++ React_2/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-red-500 text-white text-3xl p-3'>Tailwind 적용 test</div>
+  )
+}
+
+export default App
---
+++ React_2/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], /* 실제 쓰는 css 클래스만 추출하기위해 범위지정 */
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
