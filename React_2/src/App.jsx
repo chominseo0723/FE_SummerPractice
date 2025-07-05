3e108fe502213115fbc16fd402d872d4cc317d83
chominseo0723 <singer100@yu.ac.kr>
2025-07-05 21:02:25 +0900

    router설정 Home 생성

--- React_2/src/App.jsx
+++ React_2/src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import Header from './components/Header'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
 
 const App = () => {
   return (
-    <div>
-      <Header/>
-    </div>
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ React_2/src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='flex flex-row gap-3 ml-36 mt-4'>
+        <div>전체</div>
+        <div>음악</div>
+        <div>믹스</div>
+        <div>게임</div>
+        <div>뉴스</div>
+        <div>라이브</div>
+        <div>애니메이션</div>
+        <div>최근에 업로드된 동영상</div>
+        <div>감상한 동영상</div>
+        <div>새로운 맞춤 동영상</div>
+    </div>
+  )
+}
+
+export default Home
